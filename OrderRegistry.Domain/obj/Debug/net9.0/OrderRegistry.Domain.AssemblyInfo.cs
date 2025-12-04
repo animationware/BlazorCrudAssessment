70101632a7c5476a29f0e849fe145027401cb9a6
Michael Romero Ortega <michaelromeroortega@gmail.com>
2025-12-04 18:20:52 -0500

    Final changes about Documentation, Handle Errors & Code Review

--- OrderRegistry.Domain/obj/Debug/net9.0/OrderRegistry.Domain.AssemblyInfo.cs
+++ OrderRegistry.Domain/obj/Debug/net9.0/OrderRegistry.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderRegistry.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215577ea97e2fae19ef5694f0b5bfe3315728e1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderRegistry.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderRegistry.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
